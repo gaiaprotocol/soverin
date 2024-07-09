731fc811bd315453f108d7653c0635a937c32fd6
yjgaia <yjdev88@gmail.com>
2024-07-09 16:32:51 +0900

    Add jvm-libp2p

--- soverin/android/app/src/main/kotlin/com/gaiaprotocol/soverin/MainActivity.kt
+++ soverin/android/app/src/main/kotlin/com/gaiaprotocol/soverin/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.gaiaprotocol.soverin
 
 import io.flutter.embedding.android.FlutterActivity
+import io.libp2p.core.Discoverer
+import io.libp2p.core.PeerId
+import io.libp2p.core.PeerInfo
+import io.libp2p.core.Stream
+import io.libp2p.core.dsl.host
+import io.libp2p.discovery.MDnsDiscovery
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+
+    private val chatHost = host {
+    }
+
+    init {
+    }
+}
